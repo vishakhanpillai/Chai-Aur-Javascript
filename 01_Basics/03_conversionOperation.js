7ad75756e208e22d579eb38d5aa8937676eaffa1
Vishakhan Pillai <pillaivishakhan2003@gmail.com>
2025-07-17 22:36:53 +0530

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Vishakhan"
+
+//console.log(typeof score) //prints String
+
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber) // prints Number
+
+//console.log(valueInNumber) // prints NaN
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Vishakhan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "vishakhan" => true
+
+let number = 33
+let stringNumber = String(number)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
