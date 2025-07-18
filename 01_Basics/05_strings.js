f9955d6bbde8c9ad5933bf953f026d811b4e1eee
Vishakhan Pillai <pillaivishakhan2003@gmail.com>
2025-07-18 22:35:56 +0530

    added strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "Vishakhan"
+const repoCount = 20
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("RainbowSixSeige")
+
+// console.log(gameName.__proto__); // if you want to see the objects
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('6'))
+
+const newString = gameName.substring(0,4) // starting index and till where to go. it to go till 4 then it will  start from the index 0 and go till 3 (0, 1, 2 ,3)
+console.log(newString); 
+
+
+const anotherString = gameName.slice(-1, 4)
+console.log(anotherString)
+
+
+
+
