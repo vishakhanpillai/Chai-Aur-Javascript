90a4383f4f4c8a11028d472e46e42a61b18f59a4
Vishakhan Pillai <pillaivishakhan2003@gmail.com>
2025-07-19 21:54:57 +0530

    updated strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -3,7 +3,7 @@ const repoCount = 20
 
 // console.log(name + repoCount + " Value");
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
 const gameName = new String("RainbowSixSeige")
 
@@ -22,6 +22,23 @@ console.log(newString);
 const anotherString = gameName.slice(-1, 4)
 console.log(anotherString)
 
+const anotherNewString = "   vishakhan   "
+console.log(anotherNewString);
+console.log(anotherNewString.trim())
 
 
+
+
+const url = "https://www.topgun.com/pete maverick"
+
+
+console.log(url.replace(' ', '%20'))
+
+
+console.log(url.includes("pete"));
+
+
+const string = "I-feel-the-need-the-need-for-speed"
+
+console.log(string.split('-'));
 
