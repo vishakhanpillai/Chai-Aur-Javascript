728068c2fb24e0d8f18f39fe4e83c0d0b04e7233
Vishakhan Pillai <pillaivishakhan2003@gmail.com>
2025-07-22 12:25:12 +0530

    objects updated

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,53 @@
+// const tinderUser = new Object() -> singleton object
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullName:{
+        userFullName:{
+            firstName : "Vishakhan",
+            lastName : "Pillai"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName["firstName"])
+
+const obj1 = { 1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+//const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2, ...obj4}
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"v@gmail.com"
+    },
+    {
+        id:2,
+        email:"i@gmail.com"
+    },
+    {
+        id:3,
+        email:"s@gmail.com"
+    }
+]
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('nae'));
